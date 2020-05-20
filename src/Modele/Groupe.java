f9ac57869e3a90b443e3165dcba26470582f4794
pau-canet <p.canetpuig@gmail.com>
2020-05-20 10:54:59 +0200

    test de github

---
+++ src/Modele/Groupe.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+/**
+ *
+ * @author pcane
+ */
+public class Groupe {
+    
+    protected int id;
+    private String Nom;
+    private int Promotion;
+    
+    public class Promotion () {}
+    
+    
+}
---
+++ src/Modele/Type_Cours.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+/**
+ *
+ * @author pcane
+ */
+public class Type_Cours {
+    
+    protected int id; /* Declaration id */
+    private String nom; /* Declaration nom */
+    
+    public Type_Cours() {}
+    
+    
+    
+    
+}
