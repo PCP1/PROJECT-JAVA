77894c0a21a937f7393385fe6f44b7a54a4de098
pau-canet <p.canetpuig@gmail.com>
2020-05-23 20:29:18 +0200

    DAOs pas finis

--- src/DataAcessObject/DAOutilisateur.java
+++ src/DataAcessObject/DAOutilisateur.java
@@ -5,10 +5,64 @@
  */
 package DataAcessObject;
 
+import Modele.Utilisateur;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+
 /**
  *
  * @author pcane
  */
-public class DAOutilisateur {
+public class DAOutilisateur extends DAO<Utilisateur>{
+
+    public DAOutilisateur(Connection conn) {
+        super(conn);
+    }
+    
+
+    @Override
+    public boolean create(Utilisateur obj) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Utilisateur obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Utilisateur obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Utilisateur find(int id) {
+        Utilisateur utilisateur = new Utilisateur();
+        
+        try{
+            ResultSet result = this.connect.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM utilisateur WHERE IDUTILISATEUR ="+id);
+            if(result.first())
+            {
+                
+                utilisateur = new Utilisateur(id, result.getString("utilisateur.Nom"), 
+                        result.getString("Prenom"),
+                        result.getString("Email"),
+                        result.getString("Password"),
+                        result.getInt("Droit"));
+            }
+            
+        }catch (Exception e) {
+         
+            e.printStackTrace();
+        }
+        return utilisateur;
+            
+     }
+
     
 }
+
+ 
