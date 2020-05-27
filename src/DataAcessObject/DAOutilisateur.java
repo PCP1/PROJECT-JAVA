4ccce9d99e7e165f19f3491e81ad3bb329dfb3f7
pau-canet <p.canetpuig@gmail.com>
2020-05-27 13:35:51 +0200

    DAOutilisateur Aurele

--- src/DataAcessObject/DAOutilisateur.java
+++ src/DataAcessObject/DAOutilisateur.java
@@ -7,7 +7,11 @@ package DataAcessObject;
 
 import Modele.Utilisateur;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -15,20 +19,55 @@ import java.sql.ResultSet;
  * @author pcane
  */
 public class DAOutilisateur extends DAO<Utilisateur>{
+        static final String JDBC_DRIVER = "com.mysql.jdbc.Driver"; 
+        public static final String URL="jdbc:mysql://localhost/hyperplanning";
+        public static final String USERNAME="root";
+        public static final String PASSWORD="";
+        
+        private static final String DELETE_QUERY="DELETE FROM `utilisateur` WHERE utilisateur.ID_utilisateur= ?";
 
+        private static final String INSERT_QUERY="INSERT INTO utilisateur(Email, Password, Nom, Prenom, Droit) VALUES(?, ?, ?, ?, ?)";
+        
     public DAOutilisateur(Connection conn) {
         super(conn);
     }
     
 
     @Override
-    public boolean create(Utilisateur obj) {
-        return false;
+    public boolean create(Utilisateur utilisateur) {
+        try{
+            PreparedStatement ps = this.connect.prepareStatement(INSERT_QUERY);
+            ps.setString(1, utilisateur.getemail());
+            ps.setString(2, utilisateur.getpassword());
+            ps.setString(3, utilisateur.getnom());
+            ps.setString(4, utilisateur.getprenom());
+            ps.setInt(5, utilisateur.getdroit());
+            
+            ps.executeUpdate();
+            
+            System.out.println("successfull insertion");
+       
+    }       catch (SQLException ex) {
+                Logger.getLogger(DAOutilisateur.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return false;
+        
     }
 
     @Override
-    public boolean delete(Utilisateur obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean delete(Utilisateur utilisateur) {
+       try{
+            PreparedStatement ps = this.connect.prepareStatement(DELETE_QUERY);
+            ps.setInt(1, utilisateur.getid());
+            int row = ps.executeUpdate();
+            System.out.println("ligne efface: "+row);
+            
+        }   catch (SQLException ex) {
+                Logger.getLogger(DAOutilisateur.class.getName()).log(Level.SEVERE, null, ex);
+            }
+       
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return false;
     }
 
     @Override
